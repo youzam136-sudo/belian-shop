302f5604b49017f4519b75fb9f42e0f15f91d7fd
youzam136-sudo <youzam136@gmail.com>
2026-08-24 11:30:44 +0900

    Update MyPage.tsx

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -198,9 +198,13 @@ function MyPage() {
                                 쇼핑정보
                             </a>
 
-                            <a href="#">
+                            <Link to="/mypage/refund">
+                                취소/환불 내역
+                            </Link>
+
+                            <Link to="/mypage/recent">
                                 최근 본 상품
-                            </a>
+                            </Link>
 
                             <a href="#">
                                 리뷰 작성
