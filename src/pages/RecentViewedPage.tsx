5da71741897301205ddc8608f3feba8227faafb8
youzam136-sudo <youzam136@gmail.com>
2026-08-24 17:01:01 +0900

    Update RecentViewedPage.tsx

--- src/pages/RecentViewedPage.tsx
+++ src/pages/RecentViewedPage.tsx
@@ -156,7 +156,7 @@ function RecentViewedPage() {
                             <Link to="/mypage">마이페이지</Link>
                             <Link to="/mypage/refund">취소/환불 내역</Link>
                             <Link to="/mypage/recent" className="active">최근 본 상품</Link>
-                            <a href="#">리뷰 작성</a>
+                            <Link to="/mypage/review">리뷰 작성</Link>
                             <a href="#">나의 쿠폰</a>
                             <a href="#">내 정보 관리</a>
                         </nav>
