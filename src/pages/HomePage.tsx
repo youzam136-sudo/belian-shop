c7c69a97d2194260eb566e8872b32b251b56c08a
youzam136-sudo <youzam136@gmail.com>
2026-08-28 00:54:15 +0900

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,7 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import ProductSection from "../components/ProductSection";
 import BrandSection from "../components/BrandSection";
 import PromotionSection from "../components/PromotionSection";
-import KeywordSection from "../components/KeywordSection";
 import GallerySection from "../components/GallerySection";
 import Footer from "../components/Footer";
 import "../styles/home.css";
@@ -16,15 +14,9 @@ function HomePage() {
             <main>
                 <Hero />
 
-                <ProductSection />
-
                 <BrandSection />
 
-                <ProductSection />
-
                 <PromotionSection />
-
-                <KeywordSection />
 
                 <GallerySection />
             </main>
